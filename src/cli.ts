6a682c48c76b6b84022e3b4e5e9982f62ce975d2
Operations Research Engineering Software+ <alex@oresoftware.com>
2018-09-11 22:38:04 -0700

    ores/auto-commit => 'set'

--- src/cli.ts
+++ src/cli.ts
@@ -6,9 +6,7 @@ import {EVCb} from './index';
 import * as fs from 'fs';
 
 const ignoreMissingProp = process.argv.indexOf('--ignore-missing') > 0;
-
 const keyIndex = process.argv.indexOf('-k');
-
 let keyv = '';
 
 if (keyIndex > 0) {
@@ -91,28 +89,4 @@ getFileData((err, data) => {
   
 });
 
-// const expression = `obj[keyv]`;
-// try {
-//   let value = eval(expression);
-//   if (value === undefined && !ignoreMissingProp) {
-//     throw 'read.json: Accessed value was undefined - missing property/path.'
-//   }
-//   else if (value === undefined) {
-//     value = '';
-//   }
-//
-//   if (value && typeof value === 'object') {
-//     console.log(JSON.stringify(value));
-//   }
-//   else {
-//     console.log(value);
-//   }
-//
-// }
-// catch (err) {
-//   console.error(chalk.magenta(`read.json: Could not evaluate expression - could not read property: "${keyv}".`));
-//   console.error(chalk.magentaBright(`read.json: You may wish to use "'${keyv}'" instead of "${keyv}".`));
-//   throw err.message || err;
-// }
-//
 
